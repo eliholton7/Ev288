2df56e7815b197b5adad265c176e90493e799184
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-28 11:57:50 -0600

    Import problems

--- Import_Data.py
+++ Import_Data.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 path_data = '/Users/[eliholton]/Ev228_data/'
 file_name = 'KRDU_temp_188708-202508.csv'
 df = pd.read_csv(file_name)
+
 print(df)
