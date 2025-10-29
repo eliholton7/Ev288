3268e9ac94b9a1d8c180a6dbf1e076cbb8b09a08
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-29 11:29:28 -0600

    Practical 4 File 7

--- wrap_Import_Data.py
+++ wrap_Import_Data.py
@@ -3,4 +3,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fun_GHCN_data as GHCN
 
-GHCN.import_data('/Users/eliholton/GitHub/Ev228/Practical4_Data/','RSM00021432_temp_193601-202508.csv',2,3)
+GHCN.import_data('/Users/eliholton/GitHub/Ev228/Practical4_Data/','SG000061641_temp_189906-202508.csv',2,3)
