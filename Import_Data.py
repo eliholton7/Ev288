b5a88881e29822c916ded3388b34ce7377d51c16
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-29 10:05:25 -0600

    Fixing Path Data

--- Import_Data.py
+++ Import_Data.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-path_data = '/Users/[eliholton]/Ev228_data/'
+path_data = '/Users/eliholton/GitHub/Ev228/'
 file_name = 'KRDU_temp_188708-202508.csv'
-df = pd.read_csv(file_name)
+df = pd.read_csv(path_data+file_name)
 
 #print(df)
 
