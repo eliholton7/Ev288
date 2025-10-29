66637cc6f06b5e0833e3518f0529ce145e37d8c3
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-29 10:42:59 -0600

    Made function to import data

---
+++ fun_Import_Data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fun_import_data(path_data,file_name,column,row):
+    df = pd.read_csv(path_data+file_name)
+    print(df)
+    #selected_column = df[column]
+    #selected_data_point = selected_column[row] 
+    #print(selected_column)
+    #print(selected_data_point)
+    data_point = df.iloc[row,column]
+    print(data_point)
---
+++ wrap_Import_Data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fun_import_data('/Users/eliholton/GitHub/Ev228/','KRDU_temp_188708-202508.csv',2,3)
