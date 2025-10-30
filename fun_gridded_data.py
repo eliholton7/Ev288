77db3d7ff2dd4d623e39f4789b223ccc50a8934c
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-30 11:21:30 -0600

    New function for gridded data

---
+++ fun_gridded_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+def gridded_figure(path_data, file_name, spatial_variable, high_dimensional_data )
+    '''spatial_variable and high_dimensional_data represent the chosen variables, such as xy coordinates and time,
+     and should be entered as a string. '''
+    ds = xr.open_dataset(path_data+file_name)
+    #print(ds)
+    da = ds[spatial_variable]
+    #print(da)
+    #da.plot(); plt.show()
+    dmean = da.mean(high_dimensional_data)
+    #print(dmean)
+    dmean.plot(); plt.show()
