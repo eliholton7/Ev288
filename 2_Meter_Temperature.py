204b4eb42464131cf56fc1db42d57e0ebd75a29f
eliholton7 <e_holton2023@coloradocollege.edu>
2025-10-30 12:45:28 -0600

    Update 2_Meter_Temperature.py

--- 2_Meter_Temperature.py
+++ 2_Meter_Temperature.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 path_data = '/Users/eliholton/Ev228_data/'
-ds = xr.open_dataset(path_data+'Era5T2M1997-2025.nc')
+file_name = 'Era5T2M1997-2025.nc'
+ds = xr.open_dataset(path_data+file_name)
 #print(ds)
 
 da = ds['t2m']
@@ -14,3 +15,15 @@ da = ds['t2m']
 dmean = da.mean('valid_time')
 print(dmean)
 dmean.plot(); plt.show()
+
+def gridded_figure(path_data, file_name, spatial_variable, high_dimensional_data ) 
+    '''spatial_variable and high_dimensional_data represent the chosen variables, such as xy coordinates and time,
+     and should be entered as a string. '''
+    ds = xr.open_dataset(path_data+file_name)
+    #print(ds)
+    da = ds[spatial_variable]
+    #print(da)
+    #da.plot(); plt.show()
+    dmean = da.mean(high_dimensional_data)
+    #print(dmean)
+    dmean.plot(); plt.show()
