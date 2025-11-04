a4ec7772869bf685f87d22e0c3b50db6120d5a58
eliholton7 <e_holton2023@coloradocollege.edu>
2025-11-04 09:34:10 -0700

    practical 6

---
+++ practical-6-part2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+
+def import_era5(file_path='', var=''):
+    ''' Import ERA5 gridded data '''
+    ds = xr.open_dataset(file_path)
+    da = ds[var]
+
+    return da
+
+def map(in_da, out_path='', out_name=''):
+    ''' Plot map from 2D DataArray '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    lons = in_da.longitude
+    lats = in_da.latitude
+
+    image = plt.pcolormesh(lons, lats, in_da)
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+    plt.title('ERA5 Wind Speed 1980-1989')
+    cb = plt.colorbar(image, shrink=.75, orientation="vertical", pad=.02)
+    cb.set_label('meters per second')
+    plt.savefig(out_path + out_name, dpi=400)
+
+file_data = '/Users/eliholton/GitHub/Ev228/'
+file_name = 'era5_10mwind_1980-1989.nc'
+fig_path = '/Users/eliholton/GitHub/Ev228/'
+fig_name = 'era5.png'
+
+da_si10 = import_era5(file_path=file_data+file_name, var='si10')
+da_si10_timemn = da_si10.mean(dim='valid_time')
+
+map(da_si10_timemn, fig_path, fig_name)
+
+# mean_var = da_si10.mean()
+# std_var = da_si10.std()
+# max_var = da_si10.max()
+# min_var = da_si10.min()
+# print(mean_var,std_var,max_var,min_var)
---
+++ practical-6.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+file_data = '/Users/eliholton/GitHub/Ev228/'
+file_name = 'SGM00061600_temp_189201-202508.csv'
+fig_path = '/Users/eliholton/GitHub/Ev228/'
+fig_name = 'SGM.png'
+var = 'metANN'
+time_var = 'YEAR'
+
+def import_ghcn(file_path='', var=''):
+    ''' Import GHCN weather station data '''
+    df = pd.read_csv(file_path)
+    df_data = df[var]
+    df_yr = df['YEAR']
+
+    return df_data, df_yr
+def timeseries(in_df, in_x=None, out_path='', out_name=''):
+    ''' Plot timeseries from 1D dataframe '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    plt.plot(in_x, in_df, color='#dc6b2b', linewidth=2.5)
+    plt.xlabel('years')
+    plt.xlim(1892, 2025)
+    plt.ylabel('annual temperature (deg C)')
+    plt.title('Saint Louis, Senegal 1892-2025')
+    plt.savefig(out_path + out_name, dpi=400)
+
+
+df_data, df_yr = import_ghcn(file_path=file_data+file_name, var=var)
+filter_data = df_data[df_data != 999.9]
+filter_year = df_yr[df_data != 999.9]
+timeseries(filter_data, in_x=filter_year, out_path=fig_path, out_name=fig_name)
+
+mean_var = np.mean(filter_data)
+std_var = np.std(filter_data)
+max_var = np.max(filter_data)
+min_var = np.min(filter_data)
+print(mean_var,std_var,max_var,min_var)
