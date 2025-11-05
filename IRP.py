3b889a122a4cfaf1698716bf713d49bd2c7270b2
eliholton7 <e_holton2023@coloradocollege.edu>
2025-11-05 08:59:12 -0700

    started psuedocode for independent research project

---
+++ IRP.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import sys 
+
+#path data and outpath data for figure--can probably copy code from EDS project.
+
+#import function to read .cv data. Can copy code from fun_des_stats function to read a cdf file. 
+
+#set the variables that im interested in. 
+
+#plot the graph, can likely use code from the fun_gridded_data function, 
+# which also reads the cdf file so maybe fun_des_stats is not needed.
+
